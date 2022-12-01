a84c453771609c65aacb654a1e2978a4430387b3
Rasmus Leijon <rasmus.leijon@gmail.com>
2022-12-01 12:11:52 +0100

    Added day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -2,12 +2,16 @@ module Day1 where
 
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
+import Data.List (sort)
 
 type Point = (Int, Int)
 
 run :: IO ()
 run = do
     ls <- readFile "src/day1.txt"
-    print $ "Day 1, Part 1: " ++ show 1
-    print $ "Day 1, Part 2: " ++ show 2
+    let elves = map (map (\y -> read y :: Int) . lines) $ splitOn "\n\n" ls
+        sumPerElf = map sum elves
+        topThreeSum = sum $ take 3 $ reverse $ sort sumPerElf
+    print $ "Day 1, Part 1: " ++ show (maximum sumPerElf)
+    print $ "Day 1, Part 2: " ++ show topThreeSum
     print "Done"
